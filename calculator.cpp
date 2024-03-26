016fb32ed77e02556040e15e998182f74620ffce
RizkyPratama05 <rizkypp428@gmail.com>
2024-03-26 17:37:39 +0700

    menambahkan variable

---
+++ calculator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+cout << "Masukan Bilangan Pertama = ";
+cin >> bilA;
+cout << "Masukan Bilangan Kedua = ";
+cin >> bilB;
+
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan (int a, int b)
+{
+    return a - b;
+}
+
+int perkalian (int a, int b)
+{
+    return a * b;
+}
+
+float pemnagian (float a, float b)
+{
+    return a / b;
+}
