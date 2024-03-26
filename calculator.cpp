b6d7bcfa302a93fb83cb1216e07d84a56078f8ce
RizkyPratama05 <rizkypp428@gmail.com>
2024-03-26 18:25:14 +0700

    menambahkan switch case

--- calculator.cpp
+++ calculator.cpp
@@ -26,7 +26,65 @@ int perkalian (int a, int b)
     return a * b;
 }
 
-float pemnagian (float a, float b)
+float pembagian (float a, float b)
 {
     return a / b;
 }
+
+int main()
+{
+    int pilihan;
+    do
+    {
+
+      cout << "Kalkulator Sederhana" << endl;
+      cout << "====================" << endl;
+      cout << "1. penjumlahan" << endl;
+      cout << "2. pengurangan" << endl;
+      cout << "3. perkalian" << endl;
+      cout << "4. pembagian" << endl;
+      cout << "5. keluar" << endl;
+      cout << "===================" << endl;
+      cout << "Masukan menu pilihan =" << endl;
+      cin >> pilihan;
+
+      switch (pilihan)
+      {
+        case 1 :
+        input();
+        cout << "Hasil Penjumlahan = " << penjumlahan(bilA, bilB) << endl;
+        break;
+
+        case 2 :
+        input();
+        cout << "Hasil Penjumlahan = " << pengurangan(bilA, bilB) << endl;
+        break;
+
+        case 3 :
+        input();
+        cout << "Hasil Penjumlahan = " << perkalian(bilA, bilB) << endl;
+        break;
+        
+        case 4 :
+        input();
+        cout << "Hasil Penjumlahan = " << pembagian(bilA, bilB) << endl;
+        break;
+
+        case 5 :
+        break;
+        
+        default:
+        cout << "Pilihan anda salah" << endl;
+        break;
+
+        
+      }
+
+      system("pause");
+      system("CLS");
+
+    } while (pilihan );
+    
+
+}
+
